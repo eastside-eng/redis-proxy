64b192b6e890a68f0612ba1a09bc5dc2e8652c8d
ben-mays <ben@eastside.io>
2017-10-22 16:13:04 -0700

    Setting up Zap for logging and using a global logger.

--- main.go
+++ main.go
@@ -1,21 +1,18 @@
-// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
-import "github.com/eastside-eng/redis-proxy/tmp/cmd"
+import (
+	"github.com/eastside-eng/redis-proxy/cmd"
+	log "github.com/eastside-eng/redis-proxy/internal/log"
+)
 
 func main() {
+	// Wiring up logging prior to invoking any subcommands.
+	logger := log.NewLogger()
+	// We must defer Sync to ensure that all logs are output prior to exiting.
+	defer logger.Sync()
+	log.SetLogger(logger)
+
+	// The Cobra command entry point. Will parse args and find a matching
+	// command handler.
 	cmd.Execute()
 }
