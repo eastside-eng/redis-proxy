d18f9fee8b8a952d8f25b835d985c744fc7962a4
ben-mays <ben@eastside.io>
2017-10-23 00:16:54 -0700

    Fixing size of the connection buffer read to 1024 bytes for now. Adding a PING handler and basic serialization for RESP values on read.

--- internal/proxy/server.go
+++ internal/proxy/server.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"net"
 
 	"github.com/eastside-eng/redis-proxy/internal/cache"
@@ -28,13 +27,11 @@ func NewServer(cache *cache.DecayingLRUCache, redisClient *redis.Client) *Server
 
 func (s *Server) process(tcpConn net.Conn) {
 	defer tcpConn.Close()
-	reader := bufio.NewReader(tcpConn)
-	writer := bufio.NewWriter(tcpConn)
-	bytes, err := ioutil.ReadAll(reader)
-	if err != nil {
-		Logger.Warnw("Failed to parse connection bytes")
-		return
-	}
+
+	bytes := make([]byte, 1024)
+	_, err := tcpConn.Read(bytes)
+
+	Logger.Infow("Processing connection", "bytes", len(bytes), "err", err)
 	command, err := parseCommand(bytes)
 	if err != nil {
 		Logger.Warnw("Failed to parse command", "command", command)
@@ -43,9 +40,10 @@ func (s *Server) process(tcpConn net.Conn) {
 
 	resp, err := s.processCommand(command)
 	if err != nil {
-		Logger.Errorw("Failed to process command", "command", command)
+		Logger.Errorw("Failed to process command", "command", command, "err", err)
 	}
 
+	writer := bufio.NewWriter(tcpConn)
 	writer.Write(resp)
 	writer.Flush()
 }
@@ -64,6 +62,7 @@ func (s *Server) Run(port int) {
 
 	for {
 		tcpConn, err := listener.Accept()
+		Logger.Infow("Accepted new connection!")
 		if err != nil {
 			Logger.Warnf("Error accepting new connection! %v", err)
 		} else {
@@ -72,27 +71,47 @@ func (s *Server) Run(port int) {
 	}
 }
 
+var RespNIL = []byte("$-1\r\n")
+
+func RespEncodeString(str string) []byte {
+	res := fmt.Sprintf("$%d\r\n%s\r\n", len(str), str)
+	return []byte(res)
+}
+
+func RespEncodeInteger(i int) []byte {
+	res := fmt.Sprintf(":%d\r\n", i)
+	return []byte(res)
+}
+
 // Handler ...
 type handler func(cache *cache.DecayingLRUCache, redisClient *redis.Client, command *Command) ([]byte, error)
 
 var getHandler handler = func(cache *cache.DecayingLRUCache, redisClient *redis.Client, command *Command) ([]byte, error) {
 	key := command.Args[0]
 	resp, exists := cache.Get(key)
+	Logger.Infow("Invoking GET on cache", "cache-entry", resp)
 	if !exists {
 		resp := redisClient.Get(key)
-		val, err := resp.Bytes()
+		val, err := resp.Result()
+		Logger.Infow("Invoking GET on backing Redis", "redis-entry", val)
 		if err != nil {
-			return nil, err
+			return RespNIL, nil
 		}
-		cache.Add(key, val)
-		return val, nil
+		bytes := RespEncodeString(val)
+		cache.Add(key, bytes)
+		return bytes, nil
 	}
 	return resp.([]byte), nil
 }
 
+var pingHandler handler = func(cache *cache.DecayingLRUCache, redisClient *redis.Client, command *Command) ([]byte, error) {
+	return RespEncodeString("PONG"), nil
+}
+
 // A map of supported handlers for Redis commands.
 var handlers = map[string]handler{
-	"GET": getHandler,
+	"GET":  getHandler,
+	"PING": pingHandler,
 }
 
 // ProcessCommand ..
